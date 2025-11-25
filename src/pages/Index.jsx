ac8c13a99b2f27ff801c8f98843a19f7e1633ea1
keydil <fadhilfirdausadha493@gmail.com>
2025-11-25 11:06:03 +0700

    mengenal fitur filter data dan pengenalan useState

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,24 @@
 import posts from "../posts.json";
 import Article from "../components/Arcticle";
+import { useState } from "react";
 
 function Homepage() {
+    const [search, setSearch] = useState("");
+
+    const changeSearch = (event) => {
+        setSearch(event.target.value);
+        
+    };
+
   return (
     <div>
       <h1>Simple Blog</h1>
-      {posts.map((blog) => (
-        <Article title={blog.title} tags={blog.tags} author={blog.author} date={blog.date} />      
+      <div>
+        Cari Artikel : <input onChange={changeSearch}></input>
+        </div>
+        <small>Ditemukan 0 data dengan pencarian kata {search}</small>
+      {posts.map(({ title, tags, author, date }, index) => (   
+        <Article {...{ title, tags, author, date }} key={index} />
       ))}
     </div>
   );
