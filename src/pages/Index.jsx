af4416cabe8ff07c9635efe4804781b0a3c8e2f6
keydil <fadhilfirdausadha493@gmail.com>
2025-11-25 14:10:01 +0700

    Fetch API dengan useEffect

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,12 @@
 import postsData from "../posts.json";
 import Article from "../components/Arcticle";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "../components/Search";
 
 function Homepage() {
     const [posts, setPosts] = useState(postsData);
     const [totalPosts, setTotalPosts] = useState(0);
+    const [externalPosts, setExternalPosts] = useState([]);
 
     const onSearchChange = (value) => {
         const filteredPosts = postsData.filter((item) => 
@@ -15,12 +16,23 @@ function Homepage() {
         setTotalPosts(filteredPosts.length);
     };
 
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => response.json())
+            .then((json) => setExternalPosts(json));
+    }, []);
+    
   return (
     <div>
       <h1>Simple Blog</h1>
       <Search onSearchChange={onSearchChange} totalPosts={totalPosts} />
       {posts.map((props, index) => (   
         <Article {...props} key={index} />
+      ))}
+      <hr />
+      <h2>External Posts</h2>
+      {externalPosts.map((item, index) => (
+        <div key={index}>- {item.title}</div>
       ))}
     </div>
   );
