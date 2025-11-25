ab3cb8082d6771b09dc5672107d2db363d0fba5d
keydil <fadhilfirdausadha493@gmail.com>
2025-11-25 13:33:36 +0700

    menambahkan apakah sebuah artikel itu baru atau tidak

--- src/components/Arcticle.jsx
+++ src/components/Arcticle.jsx
@@ -1,8 +1,14 @@
+const ArticleStatus = ({ isNew }) => {
+  return isNew && <span>-- Baru !!!</span> ;
+};
+
 function Article(props) {
   return (
     <div>
       <h3>{props.title}</h3>
-      <small> Date : {props.date}, tag: {props.tags.join(", ")}, author: {props.author}</small>
+      <small> Date : {props.date}, tag: {props.tags.join(", ")}, author: {props.author}
+        <ArticleStatus isNew={props.isNew} />
+      </small>
     </div>
   );
 }
