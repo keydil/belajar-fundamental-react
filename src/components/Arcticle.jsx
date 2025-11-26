3c444c987b983738eff73a72c0c108d8c07bdc49
keydil <fadhilfirdausadha493@gmail.com>
2025-11-26 11:36:16 +0700

    rename context in article.jsx

--- src/components/Arcticle.jsx
+++ src/components/Arcticle.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { GlobalContext } from "../context.js";
+import { GlobalContext } from "../context";
 
 const ArticleStatus = ({ isNew }) => {
   return isNew && <span>-- Baru !!!</span> ;
